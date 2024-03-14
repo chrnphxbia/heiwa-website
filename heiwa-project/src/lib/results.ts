522ec45ba933e7d68977cee22cb3a28bf76ff845
chrnphxbia <pedro.a.farias7@gmail.com>
2024-03-14 17:31:15 -0300

    Finalizando página de resultados

---
+++ heiwa-project/src/lib/results.ts
@@ -0,0 +1,11 @@
+const results = [
+  {
+    id: "1",
+    title: "Artigo 1: Dataset Eleições 2022 (dataset paer)",
+    authors: "Livia A. dos Santos, Renata Araujo, Orlando B. Coelho, Luciano José da Silva, Ivan Carlos A. Oliveira, Ana Grasielle D. Correa",
+    date: "xx/2024",
+    link: "https://file.notion.so/f/f/2178901b-1ac8-41f2-a4e6-5f9fd3284fe9/e98b50f6-4473-465b-8133-20bf352c26b1/Brasnam2024-DatasetPaper.pdf?id=9467d0a2-76b8-499c-9441-969adff5e784&table=block&spaceId=2178901b-1ac8-41f2-a4e6-5f9fd3284fe9&expirationTimestamp=1710547200000&signature=bH_GPQm0ujtZVO2B9IbeoEclxEFGL_E98m9jxkovSjg&downloadName=Brasnam2024-DatasetPaper.pdf",
+  },  
+];
+
+export default results;
